af193903f063d0dbe49d8daa5693845944a92812
gabrielcgaldino <gabrielcgaldiino@gmail.com>
2022-06-11 18:14:05 -0300

    Log Create|Update & Wallet Colection sendo criados

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,14 +4,9 @@ import { getCustomRepository } from "typeorm";
 import UsersRepository from "../repositories/UsersRepository";
 import CreateUserService from "../services/CreateUserService";
 
+
 const usersRouter = Router()
 
-usersRouter.get('/', async (request, response)=> {
-    const usersRepository = getCustomRepository(UsersRepository)
-    const users = await usersRepository.find()
-
-    return response.json(users)
-})
 usersRouter.post('/', async(request, response)=> {
     try {
         const 
@@ -24,8 +19,8 @@ usersRouter.post('/', async(request, response)=> {
             password,
         } = request.body
 
-        const createUser = new CreateUserService()
 
+        const createUser = new CreateUserService()
         const user = await createUser.execute({
             name,
             age,
